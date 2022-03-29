e4b65498ea5710b923d1e7c8a788ed3e6d95ef40
Jamie Smith <thejamiemsmith@gmail.com>
2022-03-29 14:13:46 -0600

    deployment ready

--- server/controller.js
+++ server/controller.js
@@ -72,7 +72,7 @@ module.exports = {
             description: req.body.desc,
             image: req.body.image
         }).then(function (posts) {
-            res.redirect('http://127.0.0.1:5501/public/index.html')
+            res.redirect('/public/index.html')
         })
 
     }
