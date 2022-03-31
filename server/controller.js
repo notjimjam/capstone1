e99d6b18a5e4c807eba9dd7d36b2e573c20a0fc7
Jamie Smith <thejamiemsmith@gmail.com>
2022-03-31 12:16:27 -0600

    edited endpoints

--- server/controller.js
+++ server/controller.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.CONNECTION_STRING, {
+const { CONNECTION_STRING } = process.env
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
